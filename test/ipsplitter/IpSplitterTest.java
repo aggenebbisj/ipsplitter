e4b31bbe171279bfd26da8d609eb96d86cb1cad0
Remko de Jong <aggenebbisj@gmail.com>
2015-04-02 22:35:14 +0200

    initial commit

---
+++ test/ipsplitter/IpSplitterTest.java
@@ -0,0 +1,39 @@
+
+package ipsplitter;
+
+import java.util.Arrays;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class IpSplitterTest {
+    private final String ip;
+    private final String expected;
+
+    public IpSplitterTest(String ip, String expected) {
+        this.ip = ip;
+        this.expected = expected;
+    }
+    
+    @Test
+    public void testIps() {
+        assertThat(new IpSplitter().split(ip), is(expected));
+    }
+    
+    @Parameterized.Parameters
+    public static Iterable data() {
+        return Arrays.asList(
+                new Object[][] {
+                    { "00192.00168.00123.000001", "192.168.123.1" },
+                    { "192.168.123.1", "192.168.123.1" },
+                    { "0000000192.168.123.01", "192.168.123.1" },
+                    { "000192.00168.00123.000001", "192.168.123.1" },
+                    { "0000192.00168.00123.0000010", "192.168.123.10" },
+                    { "0192.00168.000123.00001", "192.168.123.1" },
+                }
+        );
+    }
+}
